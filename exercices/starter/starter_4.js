d7eea12d6819bb9ee142c5c72c9a2219ea96809c
razmiO <thomas.cuesta31@gmail.com>
2024-11-04 12:24:43 +0100

    fix stop var

---
+++ exercices/starter/starter_4.js
@@ -0,0 +1,15 @@
+// Exercice name : condition
+// @ts-check
+// --
+import Game from "../../Game.js";
+import API_KEY from "../../secret.js";
+
+const exoCode = "STARTER_6";
+
+const game = new Game(API_KEY, exoCode);
+
+game.start();
+
+const stop = 50;
+
+game.output(new Array(stop).fill(() => 0).reduce((acc, _, i) => acc + i + 1, 0));
