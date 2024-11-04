2a81ec037c9a56cb1a0b065e8695b7e554cf7002
razmiO <thomas.cuesta31@gmail.com>
2024-11-04 10:37:13 +0100

    add starter_1.js

---
+++ exercices/starter/starter_1.js
@@ -0,0 +1,10 @@
+// Exercice name : Utilisation d'une fonction
+// @ts-check
+// --
+import Game from "../../Game.js";
+import API_KEY from "../../secret.js";
+
+const exoCode = "STARTER_6";
+
+const game = new Game(API_KEY, exoCode);
+game.start();
